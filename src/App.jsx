9725d20cc40adb1914665ed5251168960fad43ff
Tyren <tyrenrhinehart@gmail.com>
2024-04-10 17:50:38 -0700

    table no longer breaks its container, font, colors, margins etc are now more inline with the apps simplistic nature

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import FileHasherUI from "./hasher/ui/hasherUI";
 function App() {
   const styles= {
    container:{ fontFamily:'sans-serif',textAlign:'center'},
-    mainBox : {backgroundImage: "linear-gradient(#FFFFFF,#D5D5D3)",minHeight:'50vh',border:'1px solid grey',margin:'4% 6%'},
-    font : {color:'grey'}
+    mainBox : {backgroundImage: "linear-gradient(#FFFFFF,#D5D5D3)",minHeight:'50vh',border:'1px solid grey',margin:'4% 6%'}
   }
   return (
     <div style={styles.container}>
