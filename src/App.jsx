890dc4bc3b861f499dc007e3aba7419c76afda7a
Tyren <tyrenrhinehart@gmail.com>
2024-04-10 17:28:21 -0700

    remove header and footer, change title font color to black, change height to minHeight for hasher container

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import FileHasherUI from "./hasher/ui/hasherUI";
 
 function App() {
   const styles= {
-   container:{ fontFamily:'sans-serif',textAlign:'center',display:'flex',flexDirection:'column',alignItems:'stretch',minHeight:'100vh',minWidth:'100%'},
-    headers : {backgroundImage: "linear-gradient(#004C98,#00264C)",height:'47px', flexShrink:0},
-    main :{flexGrow:1, flexShrink:0},
-    mainBox : {backgroundImage: "linear-gradient(#FFFFFF,#D5D5D3)",height:'50vh',border:'1px solid grey',margin:'4% 6%'},
+   container:{ fontFamily:'sans-serif',textAlign:'center'},
+    mainBox : {backgroundImage: "linear-gradient(#FFFFFF,#D5D5D3)",minHeight:'50vh',border:'1px solid grey',margin:'4% 6%'},
     font : {color:'grey'}
   }
   return (
     <div style={styles.container}>
-      <header style={styles.headers}/>
-     <div style={styles.main}>
        <div style={styles.mainBox}>
-         <h1 style={styles.font}>Basic File Hasher</h1>
+         <h1 >Basic File Hasher</h1>
         <FileHasherUI />
       </div>
-      </div>
-      <footer  style={styles.headers}/>
     </div>
   );
 }
